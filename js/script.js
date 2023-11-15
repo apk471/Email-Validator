d984a06bf7ab631ff8f64b2fdf21864b2b14732d
apk471 <ayush.amin2022@vitstudent.ac.in>
2023-11-15 19:04:32 +0530

    Added the main page

---
+++ js/script.js
@@ -0,0 +1,31 @@
+console.log("Hello");
+
+const element = document.getElementById("addEvent");
+element.addEventListener("click", myFunction);
+
+async function myFunction(e) {
+  console.log("Clicked");
+  e.preventDefault();
+  // const apikey = "ema_live_qJoaLHjuQ0v6LKzk2wkhuv690s5KlCEJkLwdzkzq";
+  let email = document.getElementById("email").value;
+  let str = ``;
+
+  try {
+    let url = `https://api.emailvalidation.io/v1/info?apikey=ema_live_qJoaLHjuQ0v6LKzk2wkhuv690s5KlCEJkLwdzkzq&email=${email}`;
+    let res = await fetch(url);
+    let result = await res.json();
+
+    for (let key in result) {
+      if (String(result[key]).length !== 0) {
+        let test = `<div>${key}: ${result[key]}</div>`;
+        str += test;
+      }
+    }
+
+    const placehold = document.getElementById("test");
+    placehold.innerHTML = str;
+  } catch (error) {
+    console.error("Error:", error);
+    // Handle the error (e.g., display an error message)
+  }
+}
